9755faf36cc60d0c951c7d90bca2ceee9a953386
Giulio <giulio.rondon@student.hu.nl>
2019-03-07 14:08:01 +0100

    rest service not working

--- src/main/java/com/iac/controller/AanbiedingController.java
+++ src/main/java/com/iac/controller/AanbiedingController.java
@@ -1,15 +1,20 @@
 package com.iac.controller;
 
+
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.iac.model.Aanbieding;
 import com.iac.service.AanbiedingService;
 
+@RestController
+@RequestMapping("/aanbiedingen")
 public class AanbiedingController {
     private final AanbiedingService aanbiedingService;
     
--- src/main/java/com/iac/controller/BestellingsregelController.java
+++ src/main/java/com/iac/controller/BestellingsregelController.java
@@ -6,10 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.iac.model.Bestellingsregel;
 import com.iac.service.BestellingsregelService;
 
+@RestController
+@RequestMapping("/bestellingsregels")
 public class BestellingsregelController {
     private final BestellingsregelService bestellingsregelService;
     
