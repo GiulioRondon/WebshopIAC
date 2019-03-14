712108db77be7fbfc6fb388b40b66a14da4d9f12
justHeel <justink_jamanika@hotmail.com>
2019-03-14 13:54:52 +0100

    getProductbyIdFunction

--- src/main/resources/static/fetchFunctions.js
+++ src/main/resources/static/fetchFunctions.js
@@ -7,7 +7,7 @@ function getProducten() {
             {
             	//de href in de a tag moet een link worden met een pathparameter die het id van het product moet zijn (jsonArray[i].productid)
             	console.log(jsonArray);
-            	document.getElementById("products").innerHTML += "<a href='ProductPage.html?id="+jsonArray[i].productID"'><span class='explain'><img src='"+jsonArray[i].afbeelding+"' alt='"+jsonArray[i].afbeelding+"' width='400px'></span></a>"
+            	document.getElementById("products").innerHTML += "<a href='ProductPagina.html?id="+jsonArray[i].productID+"'><span class='explain'><img src='"+jsonArray[i].afbeelding+"' alt='"+jsonArray[i].afbeelding+"' width='400px'></span></a>"
     			console.log(jsonArray[i].naam);
             }
  })
---
+++ src/main/resources/static/getProductbyIdFunction.js
@@ -0,0 +1,16 @@
+ function getProductbyId(id){
+	//product
+	fetch("/producten/" + id)
+	.then(response => response.json())
+	.then(function(json){
+		document.getElementById("myimage").src = json.afbeelding;
+		document.getElementById("ProductNaam").innerHTML = json.naam;
+		document.getElementyById("ProductPrijs").innerHTML = json.prijs;
+	})
+ }
+ 
+	var url_string = window.location.href;
+	var url = new URL(url_string);
+
+	var id = url.searchParams.get("id"); 
+ getProductbyId(id);
